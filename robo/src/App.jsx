a210f4220a9565d31731b2e695d8083f5cbf5022
Luciano Lawson <lucianoglawson@yahoo.com>
2024-04-16 17:37:42 -0500

    test

--- robo/src/App.jsx
+++ robo/src/App.jsx
@@ -16,8 +16,7 @@ function App() {
   const [messages, setMessages] = useState([{ message: "Hello, I'm Robo!", sender: "Robo", direction: "incoming" }]);
   const [isLoading, setIsLoading] = useState(true); // Set initial loading state to true
   
-  // Call the useVantaEffect hook to get the reference
-  const mountRef = useVantaEffect();
+
 
   useEffect(() => {
     setTimeout(() => {
@@ -66,38 +65,35 @@ function App() {
   }
 
   return (
-    <div className="App" ref={mountRef}>
-      {isLoading && (
-        <div className="loading-screen">
-          <ThreeText />
-        </div>
-      )}
-      {!isLoading && ( // Render the app content when isLoading is false
-        <div className="chat-container">
-          <MainContainer>
-            <ChatContainer>
-              <ConversationHeader>
-                <Avatar name="Robo" size="md" src="/robo-icon.svg" status="available" />
-              </ConversationHeader>
-              <MessageList typingIndicator={typing ? <TypingIndicator content="Robo is typing" /> : null}>
-                {messages.map((message, index) => (
-                  <Message key={index} model={message} />
-                ))}
-              </MessageList>
-              <MessageInput placeholder="Type message here" onSend={handleSend} />
-            </ChatContainer>
-          </MainContainer>
-          <div className="luciano">
-            <h5>&copy; 2024 Made with &hearts; by Luciano Lawson</h5>
-          </div>
-          <footer className="socials">
-            <a href="https://github.com/LucianoLawson/robo-ai" className="github-link"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
-            <a href="https://www.linkedin.com/in/luciano-lawson/" className="linkedin-link"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-            <a href="https://lucianolawson.vercel.app/" className="website-link"><FontAwesomeIcon icon={faCode} size="2x" /></a>
-          </footer>
-        </div>
-      )}
+<div className="App">
+  {isLoading && <div className="loading-screen"><ThreeText /></div>}
+  {!isLoading && (
+    <div className="chat-container">
+      <MainContainer>
+        <ChatContainer>
+          <ConversationHeader>
+            <Avatar name="Robo" size="md" src="/robo-icon.svg" status="available" />
+          </ConversationHeader>
+          <MessageList typingIndicator={typing ? <TypingIndicator content="Robo is typing" /> : null}>
+            {messages.map((message, index) => (
+              <Message key={index} model={message} />
+            ))}
+          </MessageList>
+          <MessageInput placeholder="Type message here" onSend={handleSend} />
+        </ChatContainer>
+      </MainContainer>
+      <div className="luciano">
+        <h5>&copy; 2024 Made with &hearts; by Luciano Lawson</h5>
+      </div>
+      <footer className="socials">
+        <a href="https://github.com/LucianoLawson/robo-ai" className="github-link"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+        <a href="https://www.linkedin.com/in/luciano-lawson/" className="linkedin-link"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+        <a href="https://lucianolawson.vercel.app/" className="website-link"><FontAwesomeIcon icon={faCode} size="2x" /></a>
+      </footer>
     </div>
+  )}
+</div>
+
   );
 }
 
