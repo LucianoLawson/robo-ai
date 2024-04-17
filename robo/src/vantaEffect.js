d7f9d087334f5486363bb1bb58ecebacc19965d1
Luciano Lawson <lucianoglawson@yahoo.com>
2024-04-16 21:49:33 -0500

    testing scss for mobile friendly

--- robo/src/vantaEffect.js
+++ robo/src/vantaEffect.js
@@ -2,33 +2,33 @@ import { useEffect, useRef } from 'react';
 import NET from 'vanta/dist/vanta.net.min.js';
 
 const useVantaEffect = () => {
-  const vantaEffect = useRef(null);
+    const vantaEffect = useRef(null);
 
-  useEffect(() => {
+    useEffect(() => {
     const width = window.innerWidth;
     const height = window.innerHeight;
 
     vantaEffect.current = NET({
-      el: document.body,
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: height,
-      minWidth: width,
-      scale: 1.00,
-      scaleMobile: 1.00,
-      color: 0x7067ff,
-      backgroundColor: 0x000000
+    el: document.body,
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: height,
+    minWidth: width,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x7067ff,
+    backgroundColor: 0x000000
     });
 
     return () => {
-      if (vantaEffect.current) {
+        if (vantaEffect.current) {
         vantaEffect.current.destroy();
-      }
+        }
     };
-  }, []);
+    }, []);
 
-  return vantaEffect;
+    return vantaEffect;
 };
 
 export default useVantaEffect;
